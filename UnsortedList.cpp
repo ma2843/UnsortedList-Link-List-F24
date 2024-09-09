73884640cb8e4cde42b10ef3f73b44179a669b8e
ma2843 <nsolomon@horizon.csueastbay.edu>
2024-09-09 07:52:48 -0700

    Updated Unsorted Linked List cpp and header file

--- UnsortedList.cpp
+++ UnsortedList.cpp
@@ -2,11 +2,22 @@
 
 template<class T>
 UnsortedList<T>::UnsortedList() {
+    head = nullptr;
+    currPos = nullptr;
+    length = 0;
+
 
 }
 
 template<class T>
 void UnsortedList<T>::MakeEmpty() {
+    while (currPos != nullptr) {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    length = 0;
 
 }
 
@@ -17,30 +28,69 @@ bool UnsortedList<T>::IsFull() const {
 
 template<class T>
 int UnsortedList<T>::GetLength() const {
-    return 0;
+    return length;
 }
 
 template<class T>
 bool UnsortedList<T>::Contains(T someItem) {
+
+    Node *currN = head;
+
+    while (currN != nullptr)
+    {
+        if(someItem == currN->item)
+        {
+            return true;
+        }
+        currN = currN->next;
+    }
+
     return false;
 }
 
 template<class T>
-void UnsortedList<T>::PutItem(T item) {
+void UnsortedList<T>::AddItem(T item) {
+
+    Node *newNode = new Node;   // hey computer, give me a memory address on the heap
+                                // big enough to store a node
+    newHead->value = item;
+    newHead->next = head;
+
+    head = newHead;
+    ++length;  // bookkeeping variable
 
 }
 
 template<class T>
 void UnsortedList<T>::DeleteItem(T item) {
+    Node *currN = head;
+    Node *prevN = nullptr;
+
+    while (currN != nullptr) {
+        if(item == currN->item) {
+            if (currN != head)
+                prevN->next = currN->next;
+            else
+                head = currN->next;
+
+            delete currN;
+            length--;
+            return;
+        }
+        prevN = currN;
+        currN = currN->next;
+    }
 
 }
 
 template<class T>
 void UnsortedList<T>::ResetIterator() {
-
+    currPos = head;  //Start the iterator at the front of the list
 }
 
 template<class T>
 T UnsortedList<T>::GetNextItem() {
-    return T();
+    T currValue = currPos->value;       // get curr value
+    currPos = currPos->next;            // move to next item
+    return currValue;                   // return curr value
 }
--- UnsortedList.h
+++ UnsortedList.h
@@ -1,64 +1,73 @@
 #ifndef UNSORTEDLIST_H
 #define UNSORTEDLIST_H
 
-template<class T>
+template <class T>
 class UnsortedList
 {
 public:
-  UnsortedList();
-  // Constructor
-  
-  void MakeEmpty();
-  // Function: Returns the list to the empty state.
-  // Post:  List is empty.
-  
-  bool IsFull() const;
-  // Function:  Determines whether list is full.
-  // Pre:  List has been initialized.
-  // Post: Function value = (list is full)
+    UnsortedList();
+    // Constructor
 
-  int GetLength() const;
-  // Function: Determines the number of elements in list.
-  // Pre:  List has been initialized.
-  // Post: Function value = number of elements in list
+    void MakeEmpty();
+    // Function: Returns the list to the empty state.
+    // Post:  List is empty.
 
-  bool Contains(T someItem);
-  // Function: Determines if someItem is in the list.
-  // Pre:  List has been initialized.
-  // Post: If there is an element someItem whose key matches
-  //       item's key, then the function returns true.
-  // 	     otherwise it will return false. List is unchanged.
+    bool IsFull() const;
+    // Function:  Determines whether list is full.
+    // Pre:  List has been initialized.
+    // Post: Function value = (list is full)
 
-  void AddItem(T item);
-  // Function: Adds item to list.
-  // Pre:  List has been initialized.
-  //       List is not full.
-  // Post: item is in list.
+    int GetLength() const;
+    // Function: Determines the number of elements in list.
+    // Pre:  List has been initialized.
+    // Post: Function value = number of elements in list
 
-  void DeleteItem(T item);
-  // Function: Deletes the first element whose key matches item's key.
-  // Pre:  ???
-  // Post: ???    
+    bool Contains(T someItem);
+    // Function: Determines if someItem is in the list.
+    // Pre:  List has been initialized.
+    // Post: If there is an element someItem whose key matches
+    //       item's key, then the function returns true.
+    // 	     otherwise it will return false. List is unchanged.
 
-  void ResetIterator();
-  // Function: Initializes current position for an iteration through the list.
-  // Pre:  List has been initialized.
-  // Post: Current position is prior to list.
+    void AddItem(T item);
+    // Function: Adds item to list.
+    // Pre:  List has been initialized.
+    //       List is not full.
+    // Post: item is in list.
 
-  T GetNextItem();
-  // Function: Gets the next element in list.
-  // Pre:  List has been initialized and has not been changed since last call.
-  //       Current position is defined.
-  //       Element at current position is not last in list.
-  //	     
-  // Post: Current position is updated to next position.
-  //       item is a copy of element at current position.
+    void DeleteItem(T item);
+    // Function: Deletes the first element whose key matches item's key.
+    // Pre:  ???
+    // Post: ???
+
+    void ResetIterator();
+    // Function: Initializes current position for an iteration through the list.
+    // Pre:  List has been initialized.
+    // Post: Current position is prior to list.
+
+    T GetNextItem();
+    // Function: Gets the next element in list.
+    // Pre:  List has been initialized and has not been changed since last call.
+    //       Current position is defined.
+    //       Element at current position is not last in list.
+    //
+    // Post: Current position is updated to next position.
+    //       item is a copy of element at current position.
 
 private:
-  int length;
+    struct Node {
+        T value;
+        Node* next;
+    };
 
-  // Need to create the head... and a node definition
-  // Also need something to track the iterator position
+    int length;
+
+    Node *head;
+    Node *currPos;  // for iterator
+
+
+    // Need to create the head... and a node definition
+    // Also need something to track the iterator position
 };
 
 #include "UnsortedList.cpp"
